91e34c852aab2dd86df211cfb4d06406081fc743
Precious Edmund <edmundprecious23@gmail.com>
2023-02-22 23:52:37 +0100

    update

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py migrate
 
-python manage.py createsuperuser --no-input --username the_username_to_want_to_use
+
+python manage.py createsuperuser --email --no-input --username the_username_to_want_to_use
+python manage.py migrate
