3daf519af432d3832380865ebf5b1cf8ffdfa538
Precious Edmund <edmundprecious23@gmail.com>
2023-02-23 21:47:24 +0000

    Update

--- NoteApi/settings.py
+++ NoteApi/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     
     'drf_yasg',
+    
+    'preciousnoteapi.onrender.com',
 ]
 
 MIDDLEWARE = [
--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 
 
 python manage.py migrate
-python manage.py createsuperuser --username myusername --email edmundprecious23@gmail.com
+
