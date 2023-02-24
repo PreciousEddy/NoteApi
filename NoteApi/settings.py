c5e1e9f9f4119d37f433f2240c6753fff3fd118e
Precious Edmund <edmundprecious23@gmail.com>
2023-02-24 20:30:50 +0000

    push

--- NoteApi/settings.py
+++ NoteApi/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,9 +32,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'rest_framework.authtoken',
-    
-    'rest_framework_swagger',
-    
+      
     'drf_yasg',
     
     
@@ -116,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
